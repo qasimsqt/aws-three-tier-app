a7a6c1fb11c761224b038375329399f318a6fe9b
qasim22 <qasimsqt@gmail.com>
2025-08-11 07:34:30 +0500

    Initial commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cqxwy4i0407j.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Qasim321',
+    DB_DATABASE : 'webappdb'
+});
