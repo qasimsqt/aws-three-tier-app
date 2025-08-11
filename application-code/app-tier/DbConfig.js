35a9fe5dbeef96d801b0e15cc72179c6170c9168
qasim22 <124474284+qasimsqt@users.noreply.github.com>
2025-08-11 09:12:37 +0500

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-1.cqxwy4i0407j.us-east-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'Qasim321',
+    DB_HOST : '',
+    DB_USER : '',
+    DB_PWD : '',
     DB_DATABASE : 'webappdb'
 });
